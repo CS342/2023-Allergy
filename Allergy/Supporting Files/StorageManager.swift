17304e1851327ce2a8f43ac4f4c8ee46734ec099
Andy Wang <106629086+awangwang@users.noreply.github.com>
2023-02-22 17:22:13 -0800

    Added StorageManager

---
+++ Allergy/Supporting Files/StorageManager.swift
@@ -0,0 +1,31 @@
+//
+//  StorageManager.swift
+//  TemplateApplication
+//
+//  Created by Vishnu Ravi on 2/2/23.
+//
+
+import FirebaseCore
+import FirebaseStorage
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    func uploadImage(_ data: Data) {
+        let id = UUID().uuidString
+        let storage = Storage.storage()
+        let storageRef = storage.reference().child("\(id).jpg")
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"
+
+        storageRef.putData(data, metadata: metadata) { (metadata, error) in
+            if let error = error {
+                print("Error while uploading file: ", error)
+            }
+
+            if let metadata = metadata {
+                print("Metadata: ", metadata)
+            }
+        }
+    }
+}
