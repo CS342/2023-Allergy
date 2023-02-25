c250776260b59283105eb7aedfdf3348ed7815c9
sonyashijin <65740491+sonyashijin@users.noreply.github.com>
2023-02-25 01:03:46 -0800

    added ARkit image tracking and auto capture view

---
+++ AllergyModules/Sources/AllergySchedule/ImageCaptureModule/ImageTracking.swift
@@ -0,0 +1,38 @@
+//ImageTracking.swfit
+//Created by Sonya Jin
+
+import SwiftUI
+import ARKit
+
+struct ARImageTrackingView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> ARSCNView {
+        let sceneView = ARSCNView()
+        let configuration = ARImageTrackingConfiguration()
+        if let imageToTrack = ARReferenceImage(named: "trackingImage", in: Bundle.main, compatibleWith: nil) {
+            configuration.trackingImages = [imageToTrack]
+        }
+        sceneView.session.run(configuration)
+        sceneView.delegate = context.coordinator
+        return sceneView
+    }
+
+    func updateUIView(_ uiView: ARSCNView, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, ARSCNViewDelegate {
+
+        func renderer(_ renderer: ARSCNView, didAdd node: SCNNode, for anchor: ARAnchor) {
+            guard let imageAnchor = anchor as? ARImageAnchor else { return }
+            // The image you are tracking has been detected
+            let screenshot = renderer.snapshot()
+            UIImageWriteToSavedPhotosAlbum(screenshot, nil, nil, nil)
+        }
+
+    }
+
+}
+
