0b6a2e5465582da9abe3c1f692596a1cc07e762c
Raj Pabari <52271154+rajpabari@users.noreply.github.com>
2023-03-02 18:36:16 -0800

    Added imagetracking.swift to the new camera impl.

---
+++ AllergyModules/Sources/AllergySchedule/ImageTracking.swift
@@ -0,0 +1,43 @@
+//
+// This source file is part of the CS342 2023 Allergy Team Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+import ARKit
+
+struct ARImageTrackingView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> ARSCNView {
+        let sceneView = ARSCNView()
+        let configuration = ARImageTrackingConfiguration()
+        if let image = UIImage(named: "human-arm.png") {
+            let referenceImage = ARReferenceImage(image.cgImage!, orientation: .up, physicalWidth: image.size.width)
+            configuration.trackingImages = [referenceImage]
+        }
+        sceneView.session.run(configuration)
+        sceneView.delegate = context.coordinator
+        return sceneView
+    }
+
+    func updateUIView(_ uiView: ARSCNView, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, ARSCNViewDelegate {
+
+        func renderer(_ renderer: ARSCNView, didAdd node: SCNNode, for anchor: ARAnchor) {
+            guard let imageAnchor = anchor as? ARImageAnchor else { return }
+            // The image you are tracking has been detected
+            let screenshot = renderer.snapshot()
+            UIImageWriteToSavedPhotosAlbum(screenshot, nil, nil, nil)
+        }
+
+    }
+
+}
