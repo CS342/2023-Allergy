0f5c220f143086b3ee5305c282ea97aa09478c21
SJayYang <stanley.j.yang@gmail.com>
2023-03-08 10:48:45 -0800

    UI tests for existence of the text

--- AllergyUITests/GalleryViewTests.swift
+++ AllergyUITests/GalleryViewTests.swift
@@ -22,12 +22,29 @@ class GalleryViewTests: XCTestCase {
     
     
     // swiftlint:disable:next function_body_length
-            func testGalleryView() throws {
+    func testGalleryView() throws {
+                let app = XCUIApplication()
+                XCTAssertTrue(app.tabBars["Tab Bar"].buttons["Gallery"].waitForExistence(timeout: 2))
+                app.tabBars["Tab Bar"].buttons["Gallery"].tap()
+                XCTAssertTrue(app.staticTexts["day0"].waitForExistence(timeout: 2))
+                XCTAssertTrue(app.staticTexts["day2"].waitForExistence(timeout: 2))
+                XCTAssertTrue(app.staticTexts["day4"].waitForExistence(timeout: 2))
+                XCTAssertTrue(app.staticTexts["base"].waitForExistence(timeout: 2))
+                app.swipeUp()
+                XCTAssertTrue(app.staticTexts["optional"].waitForExistence(timeout: 2))
+    }
+    
+    
+    func testPhotoUpload() throws {
                 let app = XCUIApplication()
                 XCTAssertTrue(app.tabBars["Tab Bar"].buttons["Gallery"].waitForExistence(timeout: 2))
                 app.tabBars["Tab Bar"].buttons["Gallery"].tap()
                 
-                               
-                
+
+
+        
+        
+
     }
+    
 }
