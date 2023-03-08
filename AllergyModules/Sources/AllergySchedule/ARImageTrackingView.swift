1af9af8a442c27d3e149607e3eb87ace5cca1597
sonyashijin <65740491+sonyashijin@users.noreply.github.com>
2023-03-08 10:38:21 -0800

    changes to ARkit (image anchor filtering) - not functional

--- AllergyModules/Sources/AllergySchedule/ARImageTrackingView.swift
+++ AllergyModules/Sources/AllergySchedule/ARImageTrackingView.swift
@@ -37,7 +37,8 @@ class ARImageTrackingViewCoordinator: NSObject, ARSCNViewDelegate {
         }
         
         let referenceImage = imageAnchor.referenceImage
-        
+               
+     
         DispatchQueue.global(qos: .userInitiated).async {
             // Create a plane to visualize the initial position of the detected image.
             let plane = SCNPlane(
@@ -67,11 +68,7 @@ class ARImageTrackingViewCoordinator: NSObject, ARSCNViewDelegate {
                 completionHandler: {
                     // If the image anchor is still available after we run the animation we take a screenshot.
                     if imageAnchor.isTracked {
-                        self.detectedImageCounter += 1
-                        if self.detectedImageCounter == 2 {
-                            self.createScreenshot()
-                            self.detectedImageCounter = 0
-                        }
+                        self.createScreenshot()
                     }
                 }
             )
@@ -94,42 +91,62 @@ class ARImageTrackingViewCoordinator: NSObject, ARSCNViewDelegate {
     }
 }
 
-
-struct ARImageTrackingView: UIViewRepresentable {
-    @Environment(\.presentationMode) private var presentationMode
-    @Binding var image: ImageState
-    
-    func makeUIView(context: Context) -> ARSCNView {
-        let sceneView = ARSCNView()
-        let configuration = ARWorldTrackingConfiguration()
-        
+extension ARImageTrackingViewCoordinator {
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        let planeNode = SCNNode()
+        // Filter the anchors to find the ones that are image anchors, use compactMap because it skips nils
+        let imageAnchors = anchors.compactMap { $0 as? ARImageAnchor }
+       
         guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: .main) else {
             fatalError("Could not load the reference images from the asset catalogue (\"AR Resources\")")
         }
         
-        configuration.detectionImages = referenceImages
-        configuration.maximumNumberOfTrackedImages = 2
-        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-        sceneView.delegate = context.coordinator
-        
-        context.coordinator.sceneView = sceneView
-        
-        // Add close button as an overlay on top of the ARSCNView
-        let closeButton = UIButton(type: .close)
-        closeButton.addTarget(context.coordinator, action: #selector(ARImageTrackingViewCoordinator.closeButtonTapped), for: .touchUpInside)
-        sceneView.addSubview(closeButton)
-        closeButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            closeButton.topAnchor.constraint(equalTo: sceneView.topAnchor, constant: 16),
-            closeButton.trailingAnchor.constraint(equalTo: sceneView.trailingAnchor, constant: -16)
-        ])
-        
-        return sceneView
-    }
-    
-    func updateUIView(_ uiView: ARSCNView, context: Context) {}
-    
-    func makeCoordinator() -> ARImageTrackingViewCoordinator {
-        ARImageTrackingViewCoordinator(image: $image, presentationMode: presentationMode)
+        // Loop through the image anchors and check if any of them are tracking our reference image
+        for imageAnchor in imageAnchors {
+            if let referenceImage = imageAnchor.referenceImage, referenceImage.cgImage == self.referenceImage.cgImage {
+                // Update the position and transform of the plane node to match the image anchor
+                planeNode.position = SCNVector3(imageAnchor.transform.columns.3.x, imageAnchor.transform.columns.3.y, imageAnchor.transform.columns.3.z)
+                planeNode.transform = SCNMatrix4(imageAnchor.transform)
+                
+                struct ARImageTrackingView: UIViewRepresentable {
+                    @Environment(\.presentationMode) private var presentationMode
+                    @Binding var image: ImageState
+                    
+                    func makeUIView(context: Context) -> ARSCNView {
+                        let sceneView = ARSCNView()
+                        let configuration = ARWorldTrackingConfiguration()
+                        
+                        
+                        configuration.detectionImages = referenceImages
+                        configuration.maximumNumberOfTrackedImages = 2
+                        
+                        
+                        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+                        sceneView.delegate = context.coordinator
+                        
+                        context.coordinator.sceneView = sceneView
+                        
+                        // Add close button as an overlay on top of the ARSCNView
+                        let closeButton = UIButton(type: .close)
+                        closeButton.addTarget(context.coordinator, action: #selector(ARImageTrackingViewCoordinator.closeButtonTapped), for: .touchUpInside)
+                        sceneView.addSubview(closeButton)
+                        closeButton.translatesAutoresizingMaskIntoConstraints = false
+                        NSLayoutConstraint.activate([
+                            closeButton.topAnchor.constraint(equalTo: sceneView.topAnchor, constant: 16),
+                            closeButton.trailingAnchor.constraint(equalTo: sceneView.trailingAnchor, constant: -16)
+                        ])
+                        
+                        return sceneView
+                    }
+                    
+                    func updateUIView(_ uiView: ARSCNView, context: Context) {}
+                    
+                    func makeCoordinator() -> ARImageTrackingViewCoordinator {
+                        ARImageTrackingViewCoordinator(image: $image, presentationMode: presentationMode)
+                    }
+                }
+            }
+        }
     }
 }
