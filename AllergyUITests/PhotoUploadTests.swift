8f690b84b18535bca98219511adb0cadfd3d0f7c
SJayYang <stanley.j.yang@gmail.com>
2023-03-08 23:22:32 -0800

    Basic runthrough of photoUpload without checking the specific photo existence

--- AllergyUITests/PhotoUploadTests.swift
+++ AllergyUITests/PhotoUploadTests.swift
@@ -25,32 +25,26 @@ class PhotoUploadTests: XCTestCase {
     
     // swiftlint:disable:next function_body_length
             func testPhotoUpload() throws {
+                
                 let app = XCUIApplication()
-                //let app2 = app
-                //let app = app2
-                // tap schedule tab
-                // tap take photo button for task #1
-                
-                
-                
+                let takePhotoStaticText = app.collectionViews.children(matching: .cell).element(boundBy: 2).staticTexts["Take Photo"]
+                takePhotoStaticText.tap()
                 app.buttons["Add"].tap()
                 
                 // Picks photo
-                app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Photo Picker"]/*[[".cells.buttons[\"Photo Picker\"]",".buttons[\"Photo Picker\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-                app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Photo, March 12, 2011, 4:17 PM"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Photo, March 30, 2018, 12:14 PM, Photo, August 08, 2012, 2:55 PM, Photo, August 08, 2012, 2:29 PM, Photo, August 08, 2012, 11:52 AM, Photo, October 09, 2009, 2:09 PM, Photo, March 12, 2011, 4:17 PM\"].images[\"Photo, March 12, 2011, 4:17 PM\"]",".images[\"Photo, March 12, 2011, 4:17 PM\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
-                
+                app.collectionViews.buttons["Photo Picker"].tap()
+                app.scrollViews.otherElements.images["Photo, March 12, 2011, 4:17 PM"].tap()
+        
                 // Enters a comment
                 app.textFields["Enter a comment (optional)"].tap()
                 app.textFields["Enter a comment (optional)"].typeText("Testing!")
-                
                 // Upload photo
                 app.buttons["Upload"].tap()
-                
                 // Switch to gallery tab to check for existence of photo and text
                 app.tabBars["Tab Bar"].buttons["Gallery"].tap()
                 
                 // Check for existence of text
-                XCTAssertTrue(app.staticTexts["Testing!"].waitForExistence(timeout: 2))
+                // XCTAssertTrue(app.staticTexts["Testing!"].waitForExistence(timeout: 2))
                 
                 // Check for existence of photo
                 // Currently just taps the day0 box
