2c4c78e56abaf5936b57d6462ba1c8c31f61d16e
Sherry Xie <85938088+sherryycxie@users.noreply.github.com>
2023-03-09 11:07:34 -0800

    Feature/education (#19)
    Co-authored-by: Andy Wang <106629086+awangwang@users.noreply.github.com>

---
+++ AllergyModules/Sources/AllergyContacts/AfterApplicationView.swift
@@ -0,0 +1,36 @@
+//
+// This source file is part of the CS342 2023 Allergy Team Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+struct AfterApplicationView: View {
+    var body: some View {
+        afterApplicationImage
+            .resizable()
+            .aspectRatio(1.5, contentMode: .fit)
+            .clipShape(Rectangle())
+            .frame(maxWidth: 350)
+    }
+    
+    
+    private var afterApplicationImage: Image {
+        guard let imagePath = Bundle.module.path(forResource: "AfterApplication", ofType: "jpg"),
+           let image = UIImage(contentsOfFile: imagePath) else {
+            return Image(systemName: "person.fill")
+        }
+        
+        // swiftlint:disable:next accessibility_label_for_image
+        return Image(uiImage: image)
+    }
+}
+
+struct AfterApplicationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AfterApplicationView()
+    }
+}
