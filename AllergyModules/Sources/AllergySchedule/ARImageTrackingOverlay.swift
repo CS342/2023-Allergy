ee5843b7c23b6227e0b5bb996611e530dceee60d
Raj Pabari <52271154+rajpabari@users.noreply.github.com>
2023-03-10 20:47:17 -0800

    move the center function to arimagetrackingoverlay

--- AllergyModules/Sources/AllergySchedule/ARImageTrackingOverlay.swift
+++ AllergyModules/Sources/AllergySchedule/ARImageTrackingOverlay.swift
@@ -24,25 +24,22 @@ struct ARImageTrackingOverlay: View {
             ARImageTrackingView(image: $image, takeScreenshot: $takeScreenshot, imageCoordindates: $imageCoordindates)
                 .overlay {
                     ZStack(alignment: .topLeading) {
-                        ForEach(imageCoordindates, id: \.debugDescription)
-                        {imageCoordinate in
-                            Text("KEEP PHONE STILL FOR 3 SECONDS")
-                                .foregroundColor(.blue)
-                                .font(.system(size: 24))
+                        Text("KEEP PHONE STILL FOR 3 SECONDS")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .padding(.horizontal, 20)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.blue, lineWidth: 0)
+                                    .background(Color.blue)
+                            )
+                            .overlay(Text("KEEP PHONE STILL FOR 3 SECONDS").foregroundColor(.white)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 20))
                                 .fontWeight(.bold)
-                                .padding(.horizontal, 20)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.blue, lineWidth: 0)
-                                        .background(Color.blue)
-                                )
-                                .overlay(Text("KEEP PHONE STILL FOR 3 SECONDS").foregroundColor(.white)
-                                    .multilineTextAlignment(.center)
-                                    .font(.system(size: 20))
-                                    .fontWeight(.bold)
-                                    .padding(.horizontal, 20))
-                                .position(x: 200, y: 50)
-                        }
+                                .padding(.horizontal, 20))
+                            .position(x: 200, y: 50)
                     }
                 }
         }
@@ -55,8 +52,18 @@ struct ARImageTrackingOverlay: View {
                 guard !newImageCoordindates.isEmpty else {
                     return
                 }
-                // Call this when you want to take a screenshot:
-                // takeScreenshot = true
+                let screenSize = UIScreen.main.bounds.size
+                let centerCoordX = screenSize.width / 2
+                let centerCoordY = screenSize.height / 2
+                
+                for coordinate in newImageCoordindates {
+                    let xCoord = Double(coordinate.x)
+                    let yCoord = Double(coordinate.y)
+                    if xCoord >= centerCoordX - 50 && xCoord <= centerCoordX + 50 && yCoord >= centerCoordY - 50 && yCoord <= centerCoordY + 50 {
+                        takeScreenshot = true
+                        
+                    }
+                }
             }
     }
 }
