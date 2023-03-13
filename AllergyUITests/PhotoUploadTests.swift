39ed22bcf6f77bd79bd0dd944a2c3ec4def42292
SJayYang <stanley.j.yang@gmail.com>
2023-03-12 17:29:32 -0700

    Added ui-tests for each photoupload, fixed scheduling ui-test

--- AllergyUITests/PhotoUploadTests.swift
+++ AllergyUITests/PhotoUploadTests.swift
@@ -10,7 +10,6 @@ import XCTestExtensions
 import XCTHealthKit
 
 
-
 class PhotoUploadTests: XCTestCase {
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -27,12 +26,12 @@ class PhotoUploadTests: XCTestCase {
             func testPhotoUpload() throws {
                 
                 let app = XCUIApplication()
-                let takePhotoStaticText = app.collectionViews.children(matching: .cell).element(boundBy: 2).staticTexts["Take Photo"]
+                var takePhotoStaticText = app.collectionViews.staticTexts["Take Baseline Photo"]
                 takePhotoStaticText.tap()
                 app.buttons["Add"].tap()
                 // Picks photo
                 app.collectionViews.buttons["Photo Picker"].tap()
-                let testPhoto = "Photo, March 12, 2011, 4:17 PM"
+                var testPhoto = "Photo, March 12, 2011, 4:17 PM"
                 app.scrollViews.otherElements.images[testPhoto].tap()
         
                 // Enters a comment
@@ -44,7 +43,102 @@ class PhotoUploadTests: XCTestCase {
                 app.tabBars["Tab Bar"].buttons["Gallery"].tap()
                 
                 // Check for existence of photo
-                app.images[testPhoto].exists
+                XCTAssertTrue(app.collectionViews.children(matching: .cell).element(boundBy: 1).scrollViews.children(matching: .other).element(boundBy: 0).children(matching: .other).element.exists)
+                
+                // Day 0
+                app.tabBars["Tab Bar"].buttons["Schedule"].tap()
+                app.swipeUp()
+                takePhotoStaticText = app.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Take Photo After Application"]/*[[".cells.staticTexts[\"Take Photo After Application\"]",".staticTexts[\"Take Photo After Application\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+                takePhotoStaticText.tap()
+                app.buttons["Add"].tap()
+                // Picks photo
+                app.collectionViews.buttons["Photo Picker"].tap()
+                testPhoto = "Photo, August 08, 2012, 2:29 PM"
+                app.scrollViews.otherElements.images[testPhoto].tap()
+        
+                // Enters a comment
+                app.textFields["Enter a comment (optional)"].tap()
+                app.textFields["Enter a comment (optional)"].typeText("Testing!")
+                // Upload photo
+                app.buttons["Upload"].tap()
+                
+                // Switch to gallery tab to check for existence of photo and text
+                app.tabBars["Tab Bar"].buttons["Gallery"].tap()
+                
+                // Check for existence of photo
+                XCTAssertTrue(app.collectionViews.children(matching: .cell).element(boundBy: 3).scrollViews.children(matching: .other).element(boundBy: 0).children(matching: .other).element.exists)
+            
+                // DAY 2
+                
+                app.tabBars["Tab Bar"].buttons["Schedule"].tap()
+                app.swipeUp()
+                takePhotoStaticText = app.collectionViews.staticTexts["Take Photo Two Days After Application"]
+                takePhotoStaticText.tap()
+                app.buttons["Add"].tap()
+                // Picks photo
+                app.collectionViews.buttons["Photo Picker"].tap()
+                testPhoto = "Photo, October 09, 2009, 2:09 PM"
+                app.scrollViews.otherElements.images[testPhoto].tap()
+        
+                // Enters a comment
+                app.textFields["Enter a comment (optional)"].tap()
+                app.textFields["Enter a comment (optional)"].typeText("Testing!")
+                // Upload photo
+                app.buttons["Upload"].tap()
+                
+                // Switch to gallery tab to check for existence of photo and text
+                app.tabBars["Tab Bar"].buttons["Gallery"].tap()
+                
+                // Check for existence of photo
+                XCTAssertTrue(app.collectionViews.children(matching: .cell).element(boundBy: 5).scrollViews.children(matching: .other).element(boundBy: 0).children(matching: .other).element.exists)
+                // DAY 4
+                
+                app.tabBars["Tab Bar"].buttons["Schedule"].tap()
+                app.swipeUp()
+                takePhotoStaticText = app.collectionViews.staticTexts["Take Photo Two Days After Removal"]
+                takePhotoStaticText.tap()
+                app.buttons["Add"].tap()
+                // Picks photo
+                app.collectionViews.buttons["Photo Picker"].tap()
+                testPhoto = "Photo, August 08, 2012, 2:55 PM"
+                app.scrollViews.otherElements.images[testPhoto].tap()
+        
+                // Enters a comment
+                app.textFields["Enter a comment (optional)"].tap()
+                app.textFields["Enter a comment (optional)"].typeText("Testing!")
+                // Upload photo
+                app.buttons["Upload"].tap()
+                
+                // Switch to gallery tab to check for existence of photo and text
+                app.tabBars["Tab Bar"].buttons["Gallery"].tap()
+                
+                // Check for existence of photo
+                XCTAssertTrue(app.collectionViews.children(matching: .cell).element(boundBy: 7).scrollViews.children(matching: .other).element(boundBy: 0).children(matching: .other).element.exists)
+                
+                
+                // Optional
+                
+                app.tabBars["Tab Bar"].buttons["Schedule"].tap()
+                app.swipeUp()
+                takePhotoStaticText = app.collectionViews.staticTexts["Take Optional Photo"]
+                takePhotoStaticText.tap()
+                app.buttons["Add"].tap()
+                // Picks photo
+                app.collectionViews.buttons["Photo Picker"].tap()
+                testPhoto = "Photo, August 08, 2012, 2:55 PM"
+                app.scrollViews.otherElements.images[testPhoto].tap()
+        
+                // Enters a comment
+                app.textFields["Enter a comment (optional)"].tap()
+                app.textFields["Enter a comment (optional)"].typeText("Testing!")
+                // Upload photo
+                app.buttons["Upload"].tap()
+                
+                // Switch to gallery tab to check for existence of photo and text
+                app.tabBars["Tab Bar"].buttons["Gallery"].tap()
+                
+                
+                                
                 
     }
 }
